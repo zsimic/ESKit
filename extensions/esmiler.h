1fa11c9f02eed7fb3d169222e9bfd9e50d879bb0
Zoran Simic <zoran@simicweb.com>
2012-05-10 22:26:07 -0700

    Fixed ARC related error (lvalues need to be set to nil with ESRELEASE)

--- extensions/esmiler.h
+++ extensions/esmiler.h
@@ -11,7 +11,7 @@ void setEsBundle(NSString *lang);
 
 #if __has_feature(objc_arc)
 #define ESRETAIN(v)      v
-#define ESRELEASE(v)
+#define ESRELEASE(v)     v = nil
 #define ESAUTO(v)        v
 #define ES_SUPER_DEALLOC
 #else
